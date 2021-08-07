b07b229fce956f3aa6b79ca37151e0f22c35cc33
nguyensong <85287532+nguyensong@users.noreply.github.com>
2021-08-07 22:06:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Linh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao con cứ đi nhầm đường
+    Lạc vào ánh mắt của sp :))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng sp đang nghĩ đến con 😙',
+    desc: 'Phải chăng sp đang nhớ con ',
+    btnYes: 'nhớ con lắm lun <33',
+    btnNo: 'Khônggggg :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao sp lại nhớ con <3',
+    btnReply: 'Gửi cho Song <3',
+    reply: 'Lúc nào cũng nhớ con <33333333',
+    mess: 'Con biết mà 🥰. Iu sp nhiều nhiều 😘😘',
+    messDesc: 'Sp buổi sáng vui vẻ ạ :)).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://chat.zalo.me/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
